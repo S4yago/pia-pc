f80dd95e64552f023e59fdb6f90e5066b8f67dbb
Moncho96 <gera_96@live.cl>
2021-11-11 10:11:39 -0600

    feat: se agrega la funcion connscan

--- portScanning.py
+++ portScanning.py
@@ -6,3 +6,16 @@ import threading
 colorama.init()
 
 print_lock = threading.Lock()
+
+def connscan(tgtHost, tgtPort):
+    scanner = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    scanner.settimeout(1)
+    try:
+        scanner.connect((tgtHost, tgtPort))
+        scanner.close()
+        with print_lock:
+            print(Fore.WHITE + f"[{tgtPort}]" + Fore.GREEN + "Abierto")
+    except:
+        print (Fore.WHITE + f"[{tgtPort}]" + Fore.RED + "Cerrado")
+    finally:
+        scanner.close()
